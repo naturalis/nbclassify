d50abb8ff8d6216c84dc87e354be02f28b9282c2
Patrick Wijntjes <patrick.wijntjes@gmail.com>
2014-04-29 15:14:08 +0200

    Clean up converter.sh
    Remove all testing echo's

--- webapp/converter.sh
+++ webapp/converter.sh
@@ -1,9 +1,4 @@
-echo "Arg: $1"
-
 ip="$1"
-echo "IP: $ip"
-
-cat "$ip.filename.txt"
 
 #Loop through every jpg file
 cd static/uploaded_files/
@@ -15,17 +10,9 @@ do
     set $i
     var="$1.png"
     x="$1.jpg"
-   	echo "Var: $var"
-   	echo "1: $1"
-   	echo "2:$2"
-   	echo "I: $i"
-   	echo "X: $x"
-   	echo "IP: $ip"
     #Conver from jpg to png
-    echo "Convert $x to $var"
     convert "$x" "$var"
     rm "$x"
-    echo "sed -i -e 's/.jpg/.png/g' \"../../$ip.filename.txt\""
     sed -i -e 's/.jpg/.png/g' "../../$ip.filename.txt"
     rm ../../*-e
     echo "---------------------------------------------"
@@ -33,6 +20,5 @@ done
 
 for a in $ip*.png
 do
-	echo "mv $a ./$ip"
 	mv "$a" ./$ip
 done
