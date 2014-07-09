b5679218d4680770b045fe6f8d627e84a7f9a7e0
Serrano Pereira <serrano.pereira@gmail.com>
2014-07-09 16:11:37 +0200

    Merge common.py

--- script/trainer.py
+++ script/trainer.py
@@ -17,6 +17,7 @@ Tasks:
 
 import argparse
 from contextlib import contextmanager
+import csv
 import logging
 import mimetypes
 import os
@@ -30,7 +31,6 @@ import sqlalchemy.orm as orm
 from sqlalchemy.ext.automap import automap_base
 import yaml
 
-import common
 # Import the feature extraction library.
 # https://github.com/naturalis/feature-extraction
 import features as ft
@@ -195,7 +195,7 @@ def get_object(d):
     """Return an object from a dictionary."""
     if not isinstance(d, dict):
         raise TypeError("Argument 'd' is not a dictionary")
-    return common.Struct(d)
+    return Struct(d)
 
 def open_yaml(path):
     """Read a YAML file and return as an object."""
@@ -209,8 +209,8 @@ class Common(object):
 
     def set_config(self, config):
         """Set the YAML configurations object."""
-        if not isinstance(config, common.Struct):
-            raise TypeError("Configurations object must be of type common.Struct, not %s" % type(config))
+        if not isinstance(config, Struct):
+            raise TypeError("Configurations object must be of type Struct, not %s" % type(config))
 
         try:
             path = config.preprocess.segmentation.output_folder
@@ -391,7 +391,7 @@ class MakeTrainData(Common):
             fh.write( "%s\n" % "\t".join(header) )
 
             # Set the training data.
-            training_data = common.TrainData(len(header_data), len(classes))
+            training_data = TrainData(len(header_data), len(classes))
             phenotyper = Phenotyper()
             failed = []
             for im_path, im_class in images:
@@ -508,7 +508,7 @@ class MakeAnn(Common):
             raise IOError("Cannot open %s (no such file)" % train_file)
 
         # Instantiate the ANN trainer.
-        trainer = common.TrainANN()
+        trainer = TrainANN()
 
         if 'ann' in self.config:
             trainer.connection_rate = getattr(self.config.ann, 'connection_rate', 1)
@@ -537,7 +537,7 @@ class MakeAnn(Common):
             dependent_prefix = OUTPUT_PREFIX
 
         try:
-            train_data = common.TrainData()
+            train_data = TrainData()
             train_data.read_from_file(train_file, dependent_prefix)
         except ValueError as e:
             logging.error("Failed to process the training data: %s" % e)
@@ -574,7 +574,7 @@ class TestAnn(Common):
         self.ann = libfann.neural_net()
         self.ann.create_from_file(ann_file)
 
-        self.test_data = common.TrainData()
+        self.test_data = TrainData()
         try:
             self.test_data.read_from_file(test_file, dependent_prefix)
         except IOError as e:
@@ -714,8 +714,8 @@ class Phenotyper(object):
 
     def set_config(self, config):
         """Set the YAML configurations object."""
-        if not isinstance(config, common.Struct):
-            raise TypeError("Configurations object must be of type common.Struct, not %s" % type(config))
+        if not isinstance(config, Struct):
+            raise TypeError("Configurations object must be of type Struct, not %s" % type(config))
         self.config = config
 
     def __preprocess(self):
@@ -955,6 +955,204 @@ class Phenotyper(object):
         means_sds = np.array(zip(means, sds)).flatten()
 
         return np.append(means_sds, histograms)
+
+class Struct(argparse.Namespace):
+    """Return a dictionary as an object."""
+
+    def __init__(self, d):
+        for key, val in d.iteritems():
+            if isinstance(val, (list, tuple)):
+                setattr(self, key, [self.__class__(x) if \
+                    isinstance(x, dict) else x for x in val])
+            else:
+                setattr(self, key, self.__class__(val) if \
+                    isinstance(val, dict) else val)
+
+class TrainData(object):
+    """Class for storing training data."""
+
+    def __init__(self, num_input = 0, num_output = 0):
+        self.num_input = num_input
+        self.num_output = num_output
+        self.labels = []
+        self.input = []
+        self.output = []
+        self.counter = 0
+
+    def read_from_file(self, path, dependent_prefix="OUT:"):
+        """Reads training data from file.
+
+        Data is loaded from TSV file `path`. File must have a header row,
+        and columns with a name starting with `dependent_prefix` are used as
+        classification columns. Optionally, sample labels can be stored in
+        a column with name "ID". All remaining columns are used as predictors.
+        """
+        with open(path, 'r') as fh:
+            reader = csv.reader(fh, delimiter="\t")
+
+            # Figure out the format of the data.
+            header = reader.next()
+            input_start = None
+            output_start = None
+            label_idx = None
+            for i, field in enumerate(header):
+                if field == "ID":
+                    label_idx = i
+                elif field.startswith(dependent_prefix):
+                    if output_start == None:
+                        output_start = i
+                    self.num_output += 1
+                else:
+                    if input_start == None:
+                        input_start = i
+                    self.num_input += 1
+
+            if self.num_input == 0:
+                raise IOError("No input columns found in training data.")
+            if self.num_output  == 0:
+                raise IOError("No output columns found in training data.")
+
+            input_end = input_start + self.num_input
+            output_end = output_start + self.num_output
+
+            for row in reader:
+                if label_idx != None:
+                    self.labels.append(row[label_idx])
+                else:
+                    self.labels.append(None)
+                self.input.append(row[input_start:input_end])
+                self.output.append(row[output_start:output_end])
+
+            self.finalize()
+
+    def __len__(self):
+        return len(self.input)
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self.counter >= len(self.input):
+            self.counter = 0
+            raise StopIteration
+        else:
+            self.counter += 1
+            i = self.counter - 1
+            return (self.labels[i], self.input[i], self.output[i])
+
+    def append(self, input, output, label=None):
+        if isinstance(self.input, np.ndarray):
+            raise ValueError("Cannot add data once finalized")
+        if len(input) != self.num_input:
+            raise ValueError("Incorrect input array length (expected length of %d)" % self.num_input)
+        if len(output) != self.num_output:
+            raise ValueError("Incorrect output array length (expected length of %d)" % self.num_output)
+
+        self.labels.append(label)
+        self.input.append(input)
+        self.output.append(output)
+
+    def finalize(self):
+        self.input = np.array(self.input).astype(float)
+        self.output = np.array(self.output).astype(float)
+
+    def normalize_input_columns(self, alpha, beta, norm_type=cv2.NORM_MINMAX):
+        if not isinstance(self.input, np.ndarray):
+            raise ValueError("Data must be finalized before running this function")
+
+        for col in range(self.num_input):
+            tmp = cv2.normalize(self.input[:,col], None, alpha, beta, norm_type)
+            self.input[:,col] = tmp[:,0]
+
+    def normalize_input_rows(self, alpha, beta, norm_type=cv2.NORM_MINMAX):
+        if not isinstance(self.input, np.ndarray):
+            raise ValueError("Data must be finalized before running this function")
+
+        for i, row in enumerate(self.input):
+            self.input[i] = cv2.normalize(row, None, alpha, beta, norm_type).reshape(-1)
+
+    def round_input(self, decimals=4):
+        self.input = np.around(self.input, decimals)
+
+    def get_input(self):
+        return self.input
+
+    def get_output(self):
+        return self.output
+
+class TrainANN(object):
+    """Train an artificial neural network."""
+
+    def __init__(self):
+        self.ann = None
+        self.connection_rate = 1
+        self.learning_rate = 0.7
+        self.hidden_layers = 1
+        self.hidden_neurons = 8
+        self.epochs = 500000
+        self.iterations_between_reports = self.epochs / 100
+        self.desired_error = 0.0001
+        self.training_algorithm = 'TRAIN_RPROP'
+        self.activation_function_hidden = 'SIGMOID_STEPWISE'
+        self.activation_function_output = 'SIGMOID_STEPWISE'
+        self.train_data = None
+        self.test_data = None
+
+    def set_train_data(self, data):
+        if not isinstance(data, TrainData):
+            raise ValueError("Training data must be an instance of TrainData")
+        self.train_data = data
+
+    def set_test_data(self, data):
+        if not isinstance(data, TrainData):
+            raise ValueError("Training data must be an instance of TrainData")
+        if data.num_input != self.train_data.num_input:
+            raise ValueError("Number of inputs of test data must be same as train data")
+        if data.num_output != self.train_data.num_output:
+            raise ValueError("Number of output of test data must be same as train data")
+        self.test_data = data
+
+    def train(self, train_data):
+        self.set_train_data(train_data)
+
+        hidden_layers = [self.hidden_neurons] * self.hidden_layers
+        layers = [self.train_data.num_input]
+        layers.extend(hidden_layers)
+        layers.append(self.train_data.num_output)
+
+        sys.stderr.write("Network layout:\n")
+        sys.stderr.write("* Neuron layers: %s\n" % layers)
+        sys.stderr.write("* Connection rate: %s\n" % self.connection_rate)
+        if self.training_algorithm not in ('FANN_TRAIN_RPROP',):
+            sys.stderr.write("* Learning rate: %s\n" % self.learning_rate)
+        sys.stderr.write("* Activation function for the hidden layers: %s\n" % self.activation_function_hidden)
+        sys.stderr.write("* Activation function for the output layer: %s\n" % self.activation_function_output)
+        sys.stderr.write("* Training algorithm: %s\n" % self.training_algorithm)
+
+        self.ann = libfann.neural_net()
+        self.ann.create_sparse_array(self.connection_rate, layers)
+        self.ann.set_learning_rate(self.learning_rate)
+        self.ann.set_activation_function_hidden(getattr(libfann, self.activation_function_hidden))
+        self.ann.set_activation_function_output(getattr(libfann, self.activation_function_output))
+        self.ann.set_training_algorithm(getattr(libfann, self.training_algorithm))
+
+        fann_train_data = libfann.training_data()
+        fann_train_data.set_train_data(self.train_data.get_input(), self.train_data.get_output())
+
+        self.ann.train_on_data(fann_train_data, self.epochs, self.iterations_between_reports, self.desired_error)
+        return self.ann
+
+    def test(self, test_data):
+        self.set_test_data(test_data)
+
+        fann_test_data = libfann.training_data()
+        fann_test_data.set_train_data(self.test_data.get_input(), self.test_data.get_output())
+
+        self.ann.reset_MSE()
+        self.ann.test_data(fann_test_data)
+
+        return self.ann.get_MSE()
+
 
 if __name__ == "__main__":
     main()
