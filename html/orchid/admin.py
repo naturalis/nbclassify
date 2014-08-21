85ef8b670789816f01f156fe630c199f67f62a5f
Serrano Pereira <serrano.pereira@gmail.com>
2014-08-21 23:42:46 +0200

    Show related Identities on Change Photo pages in the Admin section

--- html/orchid/admin.py
+++ html/orchid/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from orchid.models import Photo
+from orchid.models import Photo, Identity
+
+class IdentityInline(admin.TabularInline):
+    model = Identity
+    extra = 0
 
 class PhotoAdmin(admin.ModelAdmin):
     list_display = ('file_name',)
     readonly_fields = ('image_tag',)
+    inlines = [IdentityInline]
 
 admin.site.register(Photo, PhotoAdmin)
