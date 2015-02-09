597b6691bc02d25c7df81e16769451b6296278e2
Serrano Pereira <serrano.pereira@gmail.com>
2015-02-09 10:36:29 +0100

    Use the first 10 characters of the md5 sum

--- html/orchid/models.py
+++ html/orchid/models.py
@@ -16,7 +16,7 @@ def get_image_path(instance, filename):
     buf = instance.image.read()
     hasher.update(buf)
     parts = os.path.splitext(filename)
-    filename_ = "%s%s" % (hasher.hexdigest(), parts[1])
+    filename_ = "%s%s" % (hasher.hexdigest()[:10], parts[1])
     path = "orchid/uploads/%%Y/%%m/%%d/%s" % (filename_,)
     return time.strftime(path)
 
