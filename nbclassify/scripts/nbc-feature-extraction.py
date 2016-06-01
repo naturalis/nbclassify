09b6a172374e80e9723f71e5e83fdd7c32dff6ca
Saskiadv <saskiadevetter@hotmail.com>
2016-06-01 14:54:13 +0200

    Add option to enter ROI in fractions of img shape.
    To determine which Region Of Interest (ROI) will be used, there are now two options:
    - in pixel units: X,Y,Width,Height (previously the only option)
    - in fractions of total image size: X1,X2,Y1,Y2
    In case of the fractions, the correct pixels will be calculated by multiplying the fraction with the image shape of that side.
    This is necessary because sometimes not all images in a directory have the same shape, but still need to use the same ROI.
    
    The functions 'check_roi' and 'split_roi' are added to use the ROI options in the correct way.

--- nbclassify/scripts/nbc-feature-extraction.py
+++ nbclassify/scripts/nbc-feature-extraction.py
@@ -55,10 +55,20 @@ def main():
              "Any existing file with the same name will be overwritten."
     )
     parser.add_argument(
-        "--roi",
+        "--roi_pix",
         metavar="x,y,w,h",
         help="Region Of Interest, expressed as X,Y,Width,Height in pixel "
-             "units."
+             "units. If both roi_pix and roi_frac are given with valid "
+             "values, only roi_frac will be used."
+    )
+    parser.add_argument(
+        "--roi_frac",
+        metavar="x1,x2,y1,y2",
+        help="Region Of Interest, expressed as X1,X2,Y1,Y2 in fractions of "
+             "total image size. X1 and Y1 start in left upper corner, X2 goes "
+             "to the right, Y2 goes to the bottom. If both roi_pix and "
+             "roi_frac are given with valid values, only roi_frac will be "
+             "used."
     )
     parser.add_argument(
         "--time",
@@ -79,13 +89,15 @@ def main():
     if not os.path.exists(args.imdir):
         raise IOError("The given path does not exist.")
 
+    what_roi = check_roi(args)
+
     # Display time and duration if wanted.
     if args.time:
         starttime = datetime.datetime.now()
         newtime = print_duration(starttime, starttime)
 
     # Extract features from images.
-    descr_dict, file_list = feature_extraction(args)
+    descr_dict, file_list = feature_extraction(args, what_roi)
     
     # Display time and duration if wanted.
     if args.time:
@@ -116,7 +128,83 @@ def print_duration(starttime, previous):
     return current
 
 
-def feature_extraction(args):
+def check_roi(args):
+    """
+    Check if a valid ROI is given to the argument parser.
+
+    Expects the result of an argument parser.
+
+    If the roi_frac argument is given, this argument is
+    transformed to a list of floats in the function 'split_roi'.
+    If the list contains four items, all items are between 0 and 1,
+    the first item is smaller than the second and the third is
+    smaller than the fourth, than the given value is valid and
+    will be used to create the ROI. The string 'Fractions' is
+    returned. If these conditions are not met, a message will
+    be displayed.
+    If the roi_pix argument is given, this argument is
+    transformed to a list of integers in the function 'split_roi'.
+    If the list contains four items, than the given value is valid and
+    will be used to create the ROI. The string 'Pixels' is returned.
+    If there are not four items in this list, a message will be
+    displayed.
+    If both arguments are omitted or if both don't have valid values,
+    no ROI will be used and 'None' is returned.
+    """
+    sys.stderr.write("Check if a valid ROI is given...\n\n")
+    if args.roi_frac:
+        fractions = split_roi(args.roi_frac, True)
+        if len(fractions) == 4 and \
+                (0 <= fractions[0] <= 1) and (0 <= fractions[1] <= 1) and \
+                (0 <= fractions[2] <= 1) and (0 <= fractions[3] <= 1) and \
+                (fractions[0] < fractions[1]) and (fractions[2] < fractions[3]):
+            sys.stderr.write("The ROI given in fractions will be used.\n\n")
+            return "Fractions"
+        else:
+            sys.stderr.write("WARNING\n"
+                             "Four fractions must be given, separated by a "
+                             "comma,\nwhere the order is X1,X2,Y1,Y2.\n"
+                             "Fractions must be at least 0 and at most 1.\n"
+                             "Keep in mind that X1 < X2 and Y1 < Y2 and that\n"
+                             "counting starts at the left upper corner.\n"
+                             "These conditions weren't met,\nso the "
+                             "ROI given in fractions can't be used.\n\n")
+    if args.roi_pix:
+        pixels = split_roi(args.roi_pix, False)
+        if len(pixels) == 4:
+            sys.stderr.write("The ROI given in pixels will be used.\n\n")
+            return "Pixels"
+        else:
+            sys.stderr.write("WARNING\n"
+                             "Four pixel-values must be given, separated by a"
+                             " comma,\nwhere the order is X,Y,Width,Height.\n"
+                             "These conditions weren't met,\nso the "
+                             "ROI given in pixels can't be used.\n\n")
+    sys.stderr.write("Images will be processed without a ROI.\n\n")
+    return None
+
+
+def split_roi(roistring, decimal):
+    """
+    Transform a string to a list of either integers
+    or floats.
+
+    Expects a string with numerical characters,
+    separated by a comma and a boolean to determine
+    if there are floats or integers in the string.
+
+    The list with either integers or floats is returned.
+    """
+    roi = roistring.split(",")
+    for x in range(len(roi)):
+        if decimal:
+            roi[x] = float(roi[x])
+        else:
+            roi[x] = int(roi[x])
+    return roi
+
+
+def feature_extraction(args, what_roi):
     """
     The function takes the result of the argument parser.
     For every image in the given path the image is read and the 
@@ -145,7 +233,7 @@ def feature_extraction(args):
                 print("%d Image %s is being processed..." % (n, filename))
                 file_list.append(filename)
 
-            img = check_roi(args, img)
+            img = apply_roi(args, what_roi, img)
             img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             ret, thresh = cv2.threshold(img_gray, 170, 255, cv2.THRESH_TRUNC)
             des = surf_keypoint_detection(thresh)
@@ -155,18 +243,24 @@ def feature_extraction(args):
     return descr_dict, file_list
 
 
-def check_roi(args, img):
+def apply_roi(args, what_roi, img):
     """
-    The function takes the result of an argument parser and
+    The function takes the result of an argument parser,
+    a string to specify what type of roi is wanted and
     an image of type nd.nparray.
-    If the roi argument is given by the user, the image
+    If a valid roi argument is given by the user, the image
     is cropped to the given values.
     The (cropped) image is returned.
     """
-    if args.roi and len(args.roi.split(",")) == 4:
-        roi = args.roi.split(",")
-        img = img[int(roi[1]): int(roi[1]) + int(roi[3]),
-                  int(roi[0]): int(roi[0]) + int(roi[2])]
+    if what_roi == "Fractions":
+        roi = split_roi(args.roi_frac, True)
+        img = img[int(img.shape[0] * roi[2]):
+                  int(img.shape[0] * roi[3]),
+                  int(img.shape[1] * roi[0]):
+                  int(img.shape[1] * roi[1])]
+    elif what_roi == "Pixels":
+        roi = split_roi(args.roi_pix, False)
+        img = img[roi[1]: roi[1] + roi[3], roi[0]: roi[0] + roi[2]]
     return img
 
 
