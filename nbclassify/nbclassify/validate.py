af8925a98576f4c8c00dfc2b0973758247188d22
Saskiadv <saskiadevetter@hotmail.com>
2016-07-25 16:04:29 +0200

    Extra argument in making test data.
    Give the 'train_dir' as extra argument to 'train_data.batch_export()' when making the test data. The codebook that was created for the train data can then be used to create the test data.

--- nbclassify/nbclassify/validate.py
+++ nbclassify/nbclassify/validate.py
@@ -146,7 +146,7 @@ class Validator(Common):
             # Make test data for this fold.
             test_samples = photo_ids[test_idx]
             train_data.set_subset(test_samples)
-            train_data.batch_export(test_dir)
+            train_data.batch_export(test_dir, train_dir)
 
             # Train neural networks on training data.
             trainer.batch_train(data_dir=train_dir, output_dir=ann_dir)
